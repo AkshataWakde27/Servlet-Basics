65e7f4984ff60c33dfa0e202ab873e61b35f0e0a
AkshataWakde27 <akshatawakde@gmail.com>
2023-10-19 23:39:19 +0530

    servlet

---
+++ servlet-basics/src/main/java/com/jsp/demo/SignUp.java
@@ -0,0 +1,25 @@
+package com.jsp.demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/signup")
+public class SignUp extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String name = req.getParameter("name");
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+		
+		System.out.println(name);
+		System.out.println(email);
+		System.out.println(password);
+	}
+}
